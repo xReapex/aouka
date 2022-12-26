6704f183c1c3efa487afbf922376bc631f18ebe5
xReapex <tr7296228@gmail.com>
2022-12-26 18:10:46 +0100

    landing page

--- src/templates/component/navbar.php
+++ src/templates/component/navbar.php
@@ -1,5 +1,5 @@
-<div class="bg-gray-100 font-sans w-full min-h-screen m-0">
-    <div class="bg-white shadow">
+<div class="bg-gray-100 font-sans m-0">
+    <div class="bg-white">
         <div class="container mx-auto px-4">
             <div class="flex items-center justify-between py-4">
                 <div>
@@ -7,13 +7,13 @@
                 </div>
 
                 <div class="hidden sm:flex sm:items-center">
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Home</a>
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Products</a>
+                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Accueil</a>
+                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Nos bateaux</a>
                 </div>
 
                 <div class="hidden sm:flex sm:items-center">
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Sign in</a>
-                    <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600">Sign up</a>
+                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Connexion</a>
+                    <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600">Créer un compte</a>
                 </div>
 
                 <div class="sm:hidden cursor-pointer">
@@ -23,6 +23,7 @@
                 </div>
             </div>
 
+            <!--
             <div class="block sm:hidden bg-white border-t-2 py-2">
                 <div class="flex flex-col">
                     <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Products</a>
@@ -34,7 +35,7 @@
                         <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">Sign up</a>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
